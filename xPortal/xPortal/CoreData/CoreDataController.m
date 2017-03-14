6152be90ecfd293bc05cbd4601d87ba273070eac
sikuij <sikuij@tujia.com>
2017-03-14 12:59:28 +0800

    link error

--- xPortal/xPortal/CoreData/CoreDataController.m
+++ xPortal/xPortal/CoreData/CoreDataController.m
@@ -8,6 +8,11 @@
 
 #import "CoreDataController.h"
 #import "CoreDataManager.h"
+#import "DepartmentMO.h"
+#import "EmployeeMO.h"
+#import "PersonMO.h"
+#import "FriendInfoMO.h"
+
 
 #define kEmployeeEntityName     @"Employee"
 #define kDepartmentEntityName   @"Department"
@@ -18,12 +23,16 @@
 
 }
 
-@property(nonatomic, strong) UIScrollView *scroll;
-@property(nonatomic, strong) UITableView *departmentTable;
-@property(nonatomic, strong) UITableView *employeeTable;
-@property(nonatomic, strong) UITableView *personTable;
-@property(nonatomic, strong) UITableView *friendTable;
-@property(nonatomic, strong) UITextField *departInput;
+@property(nonatomic, strong) UIScrollView   *scroll;
+@property(nonatomic, strong) UITableView    *departmentTable;
+@property(nonatomic, strong) UITableView    *employeeTable;
+@property(nonatomic, strong) UITableView    *personTable;
+@property(nonatomic, strong) UITableView    *friendTable;
+@property(nonatomic, strong) UITextField    *departInput;
+@property(nonatomic, strong) NSArray        *departmentArr;
+@property(nonatomic, strong) NSArray        *employeeArr;
+@property(nonatomic, strong) NSArray        *personArr;
+@property(nonatomic, strong) NSArray        *friendArr;
 @end
 
 @implementation CoreDataController
@@ -42,7 +51,7 @@
     _departInput = [[UITextField alloc] initWithFrame:CGRectMake(x, y, 200, 40)];
     [_scroll addSubview:_departInput];
     //
-    UIButton *addDepartBtn = [xViewTools createBtn:CGRectMake(x+210, y, 60, 40) bgColor:kColor_FFFFFF titleColor:kColor_000000 title:@"添加" font:kFontPF(20) target:self selector:@selector(addDepartment)];
+    UIButton *addDepartBtn = [xViewTools createBtn:CGRectMake(x+210, y, 60, 40) bgColor:kColor_FFFFFF titleColor:kColor_000000 title:@"添加" font:kFontPF(20) target:self selector:@selector(actionAddDepartment)];
     [_scroll addSubview:addDepartBtn];
     y += 20;
     //
@@ -51,9 +60,52 @@
     _departmentTable.delegate = self;
 }
 
--(void)addDepartment{
-    NSString *name = _departInput.text;
-    [[CoreDataManager sharedInstance] objectForInsert:kDepartmentEntityName];
+#pragma mark - actions
+
+-(void)actionAddDepartment{
+    NSString *title = _departInput.text;
+    DepartmentMO *dep = (DepartmentMO*)[[CoreDataManager sharedInstance] objectForInsert:kDepartmentEntityName];
+    dep.title = title;
+    [[CoreDataManager sharedInstance] save];
+    [_departmentTable reloadData];
+}
+
+#pragma mark - tableView
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if(tableView == _departmentTable){
+        if(!_departmentArr){
+            _departmentArr = [[CoreDataManager sharedInstance] executeResultByPredicate:nil limit:0 offset:0 entityName:kDepartmentEntityName descriptorKey:nil ascending:NO];
+        }
+        return _departmentArr.count;
+    }
+    return 0;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 45;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(tableView == _departmentTable){
+        static NSString *cellId = @"departmentCell";
+        UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:cellId];
+        if(!cell){
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+        }
+        cell.textLabel.text = ((DepartmentMO*)_departmentArr[indexPath.row]).title;
+        return cell;
+    }
+    return nil;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
 }
 
 @end
