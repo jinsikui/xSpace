943954aa9292b0ea823047b0792b81dab2e6a526
sikuij <sikuij@tujia.com>
2017-05-02 13:24:07 +0800

    remove files

---
+++ xPortal/xPortal/InterApp/InterAppController.h
@@ -0,0 +1,13 @@
+//
+//  InterAppController.h
+//  xPortal
+//
+//  Created by JSK on 2017/5/2.
+//  Copyright © 2017年 xSpace. All rights reserved.
+//
+
+#import "PortalBaseController.h"
+
+@interface InterAppController : PortalBaseController
+
+@end
---
+++ xPortal/xPortal/InterApp/InterAppController.m
@@ -0,0 +1,37 @@
+//
+//  InterAppController.m
+//  xPortal
+//
+//  Created by JSK on 2017/5/2.
+//  Copyright © 2017年 xSpace. All rights reserved.
+//
+
+#import "InterAppController.h"
+
+@interface InterAppController ()
+
+@end
+
+@implementation InterAppController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
